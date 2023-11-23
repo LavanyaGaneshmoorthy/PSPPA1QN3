a8e600be1d94e73b15d377a2aa790472bbf2c0fe
ProfSathishR <30977778+Professor-Sathish@users.noreply.github.com>
2023-11-23 12:33:53 +0530

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,1 +1,26 @@
+# solution.py
+#check the operators
+
+def caesar_cipher(text, shift):
+    result = ""
+    for char in text:
+        if char.isalpha():
+            start = ord('a') if char.islower() else ord('A')
+            result = chr(ord(char) - start + shift % 26 + start)
+        else:
+            result = char
+    return result
+
+def swap_and_encrypt(scroll1, scroll2, lunar_phase):
+    # Swap scrolls using tuple assignment
+    scroll1, scroll2 = scroll2, scroll1
+    
+    # Encrypt each scroll with the Caesar cipher using opposite shifts
+    shift1 = lunar_phase
+    shift2 = -lunar_phase
+    
+    encrypted_scroll1 = caesar_cipher(scroll1, shift1)
+    encrypted_scroll2 = caesar_cipher(scroll2, shift2)
+    
+    return encrypted_scroll1, encrypted_scroll2
 
