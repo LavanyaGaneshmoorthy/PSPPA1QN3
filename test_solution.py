7ebbc062013e9721a91b5b8bcb93e529c2985dce
ProfSathishR <30977778+Professor-Sathish@users.noreply.github.com>
2023-11-23 12:34:12 +0530

    Update test_solution.py

--- test_solution.py
+++ test_solution.py
@@ -1,1 +1,17 @@
+# test_solution.py
+
+from solution import swap_and_encrypt
+
+def test_swap_and_encrypt():
+    scroll1 = "Hello, Elara's Scroll!"
+    scroll2 = "Greetings, Orion's Scroll!"
+    lunar_phase = 3
+
+    result1, result2 = swap_and_encrypt(scroll1, scroll2, lunar_phase)
+
+    expected_result1 = "Khoor, Hohud'v Vfrryg!"
+    expected_result2 = "Kvhuuljohv, Syvj's Vfrryg!"
+
+    assert result1 == expected_result1
+    assert result2 == expected_result2
 
